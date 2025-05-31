12c30d4a3f7c984924ffa0d79d31df88536977ce
Saikiran Uppu <9650342+uppusaikiran@users.noreply.github.com>
2025-05-31 12:27:12 -0700

    Fix Login Credentials Error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -11,7 +11,9 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: true
+    detectSessionInUrl: true,
+    storageKey: 'huggy_auth_token',
+    storage: window.localStorage
   }
 });
 
